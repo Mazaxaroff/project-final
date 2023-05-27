fd5312a856d7034a81f233e0f4f7b0c9550cb2cd
Mazaxaroff <mzxrov@mail.ru>
2023-05-27 23:56:50 +0300

    Add Task to User method

--- src/main/java/com/javarush/jira/bugtracking/TaskService.java
+++ src/main/java/com/javarush/jira/bugtracking/TaskService.java
@@ -2,18 +2,29 @@ package com.javarush.jira.bugtracking;
 
 import com.javarush.jira.bugtracking.internal.mapper.TaskMapper;
 import com.javarush.jira.bugtracking.internal.model.Task;
+import com.javarush.jira.bugtracking.internal.model.UserBelong;
 import com.javarush.jira.bugtracking.internal.repository.TaskRepository;
+import com.javarush.jira.bugtracking.internal.repository.UserBelongRepository;
+import com.javarush.jira.bugtracking.to.ObjectType;
 import com.javarush.jira.bugtracking.to.TaskTo;
+import com.javarush.jira.login.User;
+import com.javarush.jira.login.internal.UserRepository;
 import jakarta.validation.constraints.Size;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 
 @Service
 public class TaskService extends BugtrackingService<Task, TaskTo, TaskRepository> {
-    public TaskService(TaskRepository repository, TaskMapper mapper) {
+
+    private final UserRepository userRepository;
+    private final UserBelongRepository userBelongRepository;
+    public TaskService(TaskRepository repository, TaskMapper mapper, UserRepository userRepository, UserBelongRepository userBelongRepository) {
         super(repository, mapper);
+        this.userRepository = userRepository;
+        this.userBelongRepository = userBelongRepository;
     }
 
     public List<TaskTo> getAll() {
@@ -24,5 +35,21 @@ public class TaskService extends BugtrackingService<Task, TaskTo, TaskRepository
         Task task = repository.getExisted(id);
         task.getTags().addAll(tags);
         repository.save(task);
+    }
+
+    public void addTaskToUser (Long userId, Long taskId){
+        User user = userRepository.getExisted(userId);
+        Task task = repository.getExisted(taskId);
+        UserBelong userBelong = new UserBelong();
+
+        userBelong.setUserId(userId);
+        userBelong.setObjectId(taskId);
+        userBelong.setObjectType(ObjectType.TASK);
+        if (!user.getRoles().isEmpty()) {
+            userBelong.setUserTypeCode(user.getRoles().stream().findAny().get().name());
+        }
+
+        userBelongRepository.save(userBelong);
+
     }
 }
