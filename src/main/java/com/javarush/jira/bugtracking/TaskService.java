94b964041e44cf3260ccf5e30ee6c4dd16e51f44
Mazaxaroff <mzxrov@mail.ru>
2023-05-27 22:19:15 +0300

    Add Tags to Task method

--- src/main/java/com/javarush/jira/bugtracking/TaskService.java
+++ src/main/java/com/javarush/jira/bugtracking/TaskService.java
@@ -4,9 +4,11 @@ import com.javarush.jira.bugtracking.internal.mapper.TaskMapper;
 import com.javarush.jira.bugtracking.internal.model.Task;
 import com.javarush.jira.bugtracking.internal.repository.TaskRepository;
 import com.javarush.jira.bugtracking.to.TaskTo;
+import jakarta.validation.constraints.Size;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Set;
 
 @Service
 public class TaskService extends BugtrackingService<Task, TaskTo, TaskRepository> {
@@ -16,5 +18,11 @@ public class TaskService extends BugtrackingService<Task, TaskTo, TaskRepository
 
     public List<TaskTo> getAll() {
         return mapper.toToList(repository.getAll());
+    }
+
+    public void addTagsToTask(Long id, Set<@Size(min = 2, max = 32) String> tags){
+        Task task = repository.getExisted(id);
+        task.getTags().addAll(tags);
+        repository.save(task);
     }
 }
